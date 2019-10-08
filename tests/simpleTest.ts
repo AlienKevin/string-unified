95ddbd7a314bce4d0a25fd821d41cea70540d266
Kevin Li <kevinli020508@gmail.com>
2019-10-08 19:48:32 -0400

    Start ignoring simpleTest.ts

--- tests/simpleTest.ts
+++
@@ -1,9 +0,0 @@
-import { charAt, substring, startsWith } from "../index";
-
-// console.log(charAt('谢谢你', -1));
-
-// console.log(startsWith('Iñtërnâtiônà🏋️‍o✌🤷‍🤦‍♂️🤣😒👌₦lizætiøn☃', 'Iñtërnâtiônà🏋️‍o✌'))
-
-// console.log(substring('Iñtë', 30));
-
-// console.log(startsWith('', '😎'))
